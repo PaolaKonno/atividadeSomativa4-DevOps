12ddb519401926102a14c991e3170a119f4e50af
Paola Konno <paolakonno13@gmail.com>
2025-08-28 20:49:13 -0300

    Alteração endpoint
    Alteração endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import random
 app = FastAPI()
 
 # http://127.0.0.1:8000
@@ -8,6 +9,6 @@ async def read_root():
 
 
 # http://127.0.0.1:8000/teste1
-@app.get("/teste1") 
+@app.get("/teste1")
 async def funcaoteste():
-    return{"teste" : "deu certo"}
+    return{"teste" : True, "num_aleatorio": random.randint(1,1000)}
