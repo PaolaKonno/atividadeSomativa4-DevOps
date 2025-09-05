408616f0135fead8044c7540bfd3f789186838df
Paola Konno <paolakonno13@gmail.com>
2025-09-05 12:49:03 -0300

    docker
    Incluído docker

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
 import random
+from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
+
+class Estudante(BaseModel) :
+    name:  str
+    curso: str
+    ativo: bool
+@app.get("/")
+def root():
+    return {"status": "ok", "try": ["/helloworld", "/teste1", "/docs"]}
 
 @app.get("/helloworld")
 async def read_root():
@@ -12,3 +21,14 @@ async def read_root():
 @app.get("/teste1")
 async def funcaoteste():
     return{"teste" : True, "num_aleatorio": random.randint(1,1000)}
+@app.post("/estudantes/cadastro")
+async def cadastro(estudante: Estudante):
+    return {"estudante" : estudante}
+
+@app.put("/estudantes/update/{id_estudante}")
+async def update_item(id_estudante:int):
+    return id_estudante > 0
+
+@app.delete("/estudantes/update/{id_estudante}")
+async def update_item(id_estudante:int):
+    return id_estudante > 0
