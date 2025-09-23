ebe8db5d6a153280e14e4a070e8732552be9f6d8
Paola Konno <paolakonno13@gmail.com>
2025-09-22 22:28:13 -0300

    Começando novamente

--- test/test.py
+++ test/test.py
@@ -1,50 +1,40 @@
 from src.main import *
 from unittest.mock import patch
 
+def test_create_estudante():
+    estudante_teste = Estudante(name= "Paola", curso= "Curso 1", ativo=False)
+    assert estudante_teste == create_estudante
 
-import pytest
-
-from src.main import root
-
-
-@pytest.mark.asyncio
-async def test_root():
+def test_root():
     assert root() == {"message": "Hello World"}
 
-@pytest.mark.asyncio
-async def test_root():
-    result =  await root()
-    assert result == {"message": "Hello World"}
 
-@pytest.mark.asyncio
-# helper simples para simular criação (se tiver endpoint real, use-o no lugar)
-async  def test_funcaoteste():
+# http://127.0.0.1:8000/teste1
+
+async def test_funcaoteste():
+    assert funcaoteste() == {"teste" : True, "num_aleatorio": random.randint(1,1000)}
     with patch('random.randint', return_value=12345):
-        result = await funcaoteste()
-        assert  result == {"message": "Hello World"}
+        result = funcaoteste()
+    assert result == {"teste": True, "num_aleatorio": 12345}
 
-@pytest.mark.asyncio
-async def test_create_estudante():
-    estudante_teste = Estudante(name="Paola", curso="DEVOPs", ativo=False)
-    result = await create_estudante(estudante_teste)
-    assert estudante_teste == result
+async def cadastro(estudante: Estudante):
+    return estudante
 
-@pytest.mark.asyncio
-async def test_update_estudante_negativo():
-    result = await update_estudante(-5)
-    assert not result
 
-@pytest.mark.asyncio
-async def test_update_estudante_positivo():
-    result = await update_estudante(10)
-    assert result
+async def teste_update_estudante_negativo():
+    assert not update_estudante(-5)
 
-@pytest.mark.asyncio
-async def test_delete_estudante_negativo():
-    result = await delete_estudante(-5)
-    assert not result
 
-@pytest.mark.asyncio
-async def test_delete_estudante_positivo():
-    result = await delete_estudante(5)
-    assert result
+async def teste_update_estudante_positivo():
+    assert update_estudante(5)
+
+async def teste_delete_estudante_negativo():
+    assert not delete_estudante(-5)
+
+async def teste_delete_estudante_positivo():
+    assert  delete_estudante(5)
+
+class Estudante(BaseModel) :
+    name:  str
+    curso: str
+    ativo: bool
