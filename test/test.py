d6bd339e5a9c14d79735fe3b77adc60351c8778e
Paola Konno <paolakonno13@gmail.com>
2025-09-23 13:10:17 -0300

    Correção yield - assert

--- test/test.py
+++ test/test.py
@@ -3,38 +3,30 @@ from unittest.mock import patch
 
 def test_root():
     result = root()
-    yield result
     assert result == {"Hello World!"}
 
 def test_funcaoteste():
     with patch('random.randint', return_value=12345):
         result = funcaoteste()
-        yield result
-
     assert result == {"teste": True, "num_aleatorio": 12345}
 
 def test_create_estudante():
-    estudante_teste = Estudante(name= "Paola", curso= "BSI", ativo=False)
+    estudante_teste = Estudante(name="Paola", curso="BSI", ativo=False)
     result = create_estudante(estudante_teste)
-    yield result
     assert estudante_teste == result
 
 def test_update_estudante_negativo():
     result = update_estudante(-5)
-    yield result
     assert not result
 
 def test_update_estudante_positivo():
     result = update_estudante(10)
-    yield result
     assert result
 
 def test_delete_estudante_negativo():
     result = delete_estudante(-5)
-    yield result
     assert not result
 
 def test_delete_estudante_positivo():
     result = delete_estudante(5)
-    yield result
     assert not result
